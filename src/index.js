5f16ebbd765e5f7510dd605cdb002a85b3db30d3
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2025-05-07 21:18:35 -0300

    new commit - gitfind

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client';
 import "./style.css";
 import App from './pages/Home';
 
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
